151cf83ce0f0bc4002d3a41e5124f43521ecf3bd
DILAN <dilan.vista@gmail.com>
2018-08-03 08:49:53 +0530

    initial commit

---
+++ src/main/java/lk/icoder/websockets/WebsocketsApplication.java
@@ -0,0 +1,12 @@
+package lk.icoder.websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketsApplication.class, args);
+    }
+}
